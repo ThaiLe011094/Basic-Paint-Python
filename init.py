bfdfc0c9d9449fb5bc1be0498cfea856ddd9085c
Thái Lê <charliebellow.lt@gmail.com>
2018-09-05 11:53:05 +0700

    Update init.py

--- init.py
+++ init.py
@@ -1,16 +1,13 @@
 class init:
-    def __init__(self, x1, y1, x2, y2, choice, can, master):
+    def __init__(self, x1, y1, x2, y2, m_pos, choice, can, master):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+        self.m_pos = m_pos
         self.choice = choice
         self.can = can
         self.master = master
-        # master = Tk()
-        # self.master = master
-        # can = Canvas(master, bg='dark grey', height=500, width=500)
-        # self.can = can
         print('init received pos x1 = {}, y1 = {}, x2 = {}, y2 = {}'.format(self.x1, self.y1, self.x2, self.y2))
 
     def clear(self):
@@ -27,5 +24,16 @@ class init:
 
     def dot_process(self):
         print('dot_process received pos x1 = {}'.format(self.x1))
-        while (self.x2 != self.x1) or (self.y2 != self.y1):
-            self.can.create_oval(self.x1, self.y1, self.x1, self.y1, outline='red')
+        for i in range(int(len(self.m_pos)/2)):
+            if i <= 0:
+                pass
+            else:
+                dx = self.m_pos[i*2]-self.m_pos[i*2-2]
+                dy = self.m_pos[i*2+1] - self.m_pos[i*2-1]
+                max_d = 2*max(abs(dx), abs(dy))
+                for j in range(max_d):
+                    self.can.create_oval(int(self.m_pos[i*2]+dx*j/max_d),               # x1
+                                         int(self.m_pos[i*2+1]+dy*j/max_d),             # y1
+                                         int(self.m_pos[i*2]+dx*j/max_d),               # x2
+                                         int(self.m_pos[i*2+1]+dy*j/max_d),             # y2
+                                         outline='red')
